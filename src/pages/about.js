3d22e6fcdfc836d0f23b31c9589050c2e997c37f
Jacob Mayfield <jacobmayfield899@gmail.com>
2021-10-18 14:45:54 -0400

    10/18 Assignment

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,11 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 
-const AboutPage = () => {
+const PokemonPage = () => {
   return (
-    <Layout pageTitle="About Me">
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    <Layout pageTitle="Pokemon">
     </Layout>
   )
 }
 
-export default AboutPage
+export default PokemonPage
