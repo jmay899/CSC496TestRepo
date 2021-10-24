d991666d470fc41ab7b861ca5fb5fc5c8d1662ad
Jacob Mayfield <jacobmayfield899@gmail.com>
2021-10-23 20:02:51 -0400

    Fixed blog, added pokemon

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,23 +17,9 @@ module.exports = {
       options: {
         url: `https://csc496wordpress.tldr.dev/graphql`,
         protocol: `https`,
-        hostingWPCOM: false,
-        useACF: false
+        hostingWPCOM: 'false',
+        useACF: 'false'
       }
-    },
-	`gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
-      },
     },
   ]
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,26 +3,29 @@ import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 
 const BlogPage = ({ data }) => {
-  return (
-    <Layout pageTitle="My Blog Posts">
-      <ul>
-      {
-        data.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
-        ))
-      }
-      </ul>
-    </Layout>
-  )
+	return (<Layout pageTitle="My Blog Posts">
+		<ul>
+		{
+			data.allWpPost.edges.map(({node}) => 
+			(<div>
+				<h1>{node.title}</h1>
+				<div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+			</div>
+			))}
+		</ul>
+	</Layout>
+	)
 }
 
 export const query = graphql`
   query {
-    allFile {
-      nodes {
-        name
+    allWpPost (sort: { fields: [date] } ) {
+      edges {
+          node{
+           title
+           excerpt
+           slug
+          }
       }
     }
   }
