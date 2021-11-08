a1f531d175432d03a60d390a81c3e43d084225ff
Jacob Mayfield <jacobmayfield899@gmail.com>
2021-11-08 00:29:42 -0500

    Failed theme switch attempt

---
+++ src/components/Theme/toggleLightButton.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react"
+import Layout from '../components/layout'
+import styled from 'styled-components'
+
+const LightButton = () => {
+  return (
+	<>
+	  <button type="button">Toggle Theme</button>
+	</>
+  )
+}
+
+export default LightButton
